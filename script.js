54a1a8cc93a7f32793664bbf9c2b65e5f8a5b817
Gustavo Escudeiro <gu.escudeiroti@hotmail.com>
2019-11-23 12:45:22 -0300

    adicionando funcionalidade no aquivo script.js e style.css no index.html

--- script.js
+++ script.js
@@ -2,3 +2,19 @@
 Aula5: Calculadora Flex
 */
 
+var etanol, gasolina;
+
+function calcular() {
+    etanol = parseFloat(frmflex.txtetanol.value.replace(",","."));
+    gasolina = parseFloat(frmflex.txtgasolina.value.replace(",","."));
+
+    if(etanol < 0.7 * gasolina) {
+        document.getElementById("status").src="etanol.png"
+    } else {
+        document.getElementById("status").src="gasolina.png"
+    }
+}
+
+function resetar() {
+    document.getElementById("status").src="neutro.png"
+}
